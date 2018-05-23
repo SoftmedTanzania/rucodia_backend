ad05cfd36be29421f8136223d93a36fbc968218d
Bwana Kizito <kizomanizo@gmail.com>
2018-05-23 21:06:10 +0300

    Addded SMS method.

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -284,6 +284,7 @@ class UserController extends Controller
      */
     public function sms(Request $request)
     {
-        Storage::put('sms.txt', $request);
+        // Storage::put('sms.txt', $request);
+        Storage::disk('local')->put('sms.txt', $request);
     }
 }

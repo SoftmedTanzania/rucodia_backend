ca6ce7cac5f942a32c022bb8acb91f81adbbbbc0
Bwana Kizito <kizomanizo@gmail.com>
2018-05-29 16:54:15 +0300

    Added error handling in users forms

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.admin')
 @section('content')
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <div class="row">
     <div class="col-lg-12 col-md-12">
         <div class="card">
